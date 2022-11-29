af89af0c8efb943427930a57626117bbb4f088a0
ZeehnohCodes <markgiftt@gmail.com>
2022-11-29 19:18:03 +0000

    all

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,8 @@
+!/usr/bin/python3
+# 10-add.py
+# ZeehnohCodes <markgiftt@gmail.com>
+
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 11-pow.py
+# ZeehnohCodes <markgiftt@gmail.com>
+
+
+def pow(a, b):
+    """Return a to the power of b."""
+    return (a ** b)
