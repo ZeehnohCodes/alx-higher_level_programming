2446d08bc66bb40927bfd7b02bed9dba6a7894ed
ZeehnohCodes <markgiftt@gmail.com>
2022-11-29 20:20:07 +0000

    new updates

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # 6-print_comb3.py
-# Brennan D Baraban <375@holbertonschool.com>
 
 """Print all possible different combinations of two digits in ascending order.
     The two digits must be different - 01 and 10 are considered identical.
