8889ef6109ea2bbc9e5b2a6ff038d7030212c657
ZeehnohCodes <markgiftt@gmail.com>
2022-12-08 18:41:38 +0000

    new updates

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-
-
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
+    if type(roman_string) is not str or roman_string is None:
+        return 0
 
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-    }
+    roman_letters = [
+            ['M', 1000], ['D', 500], ['C', 100], ['L', 50],
+            ['X', 10], ['V', 5], ['I', 1]
+    ]
     num = 0
+    last = 0
 
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
+    for letter in roman_string:
+        for elem in roman_letters:
+            if letter == elem[0]:
+                if last == 0 or last >= elem[1]:
+                    num += elem[1]
+                elif last < elem[1]:
+                    num += elem[1] - (last * 2)
 
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-
-        else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+                last = elem[1]
+    return num
