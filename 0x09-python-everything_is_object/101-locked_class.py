0e21edf2b2d8486869ae8280e6a5ff5fe6d2ecc4
ZeehnohCodes <markgiftt@gmail.com>
2023-01-10 18:32:45 +0000

    all

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# 101-locked_class.py
 """Defines a locked class."""
 
 
@@ -9,4 +8,4 @@ class LockedClass:
     for anything but attributes called 'first_name'.
     """
 
-    _slots_ = ["first_name"]
+    __slots__ = ["first_name"]
