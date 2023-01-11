eaa5e294e9f3df244676ee78ced357783ea8d800
ZeehnohCodes <markgiftt@gmail.com>
2023-01-11 18:19:22 +0000

    all

--- 0x0A-python-inheritance/tests/7-main.py
+++ 0x0A-python-inheritance/tests/7-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-BaseGeometry = _import_('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
 
@@ -9,14 +9,14 @@ bg.integer_validator("width", 89)
 try:
     bg.integer_validator("name", "John")
 except Exception as e:
-    print("[{}] {}".format(e._class.name_, e))
+    print("[{}] {}".format(e.__class__.__name__, e))
 
 try:
     bg.integer_validator("age", 0)
 except Exception as e:
-    print("[{}] {}".format(e._class.name_, e))
+    print("[{}] {}".format(e.__class__.__name__, e))
 
 try:
     bg.integer_validator("distance", -4)
 except Exception as e:
-    print("[{}] {}".format(e._class.name_, e))
+    print("[{}] {}".format(e.__class__.__name__, e))
